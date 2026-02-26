7ef5c18680a59dabb0bcd73236fb461acae884a4
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2026-02-26 11:28:10 -0300

    Mudando o HealthStatus para uma forma mais eficiente com record.

--- src/main/java/br/com/fiap/movies/controllers/MovieController.java
+++ src/main/java/br/com/fiap/movies/controllers/MovieController.java
@@ -8,14 +8,13 @@ import java.time.LocalDate;
 @RestController
 public class MovieController {
 
+    //record é como se fosse uma classe imutavel. Também dá pra ser criado em um arquivo separado.
+    record HealthStatus(String status, String message){};
+
+    //Jackson converte o objeto Movie pra JSON automaticamente.
     @GetMapping("/")
-    public String healthCheck() {
-        return """
-                { 
-                    "status": "ok",
-                    "message": "API is running"
-                }
-                """;
+    public HealthStatus healthCheck() {
+        return new HealthStatus("ok", "API is running");
     }
 
     @GetMapping("/movies")
@@ -29,5 +28,4 @@ public class MovieController {
                 "Romance"
         );
     }
-
 }
